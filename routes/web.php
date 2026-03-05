7e69de685138238a26e9c51dd64f21462e3b3010
Zylpheon <lrv94451@gmail.com>
2026-03-05 09:27:08 +0700

    fifth commit

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
-
-Route::view('/', 'welcome')->name('home');
-
+Route::get('/', function () {
+    return response()->file(resource_path('views/index.html'));
+})->name('home');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 });
-
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
